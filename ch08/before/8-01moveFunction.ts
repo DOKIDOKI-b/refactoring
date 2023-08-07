83aac9cb8121b503f0080be6da3b4a00d3c3737c
daal2quf <daal2quf@gmail.com>
2023-08-07 22:34:36 +0900

    fix : change initial code

--- ch08/before/8-01moveFunction.ts
+++ ch08/before/8-01moveFunction.ts
@@ -1,29 +1,35 @@
-function trackSummary(points : Array<number>) {
+function trackSummary(points: Array<number>) {
+  function calculateDistance(points: Array<number>) {
+    let result = 0;
+    for (let i = 1; i < points.length; i++) {
+      result += distance(points[i - 1], points[i]);
+    }
+    return result;
+  }
+
+  function distance(p1: number, p2: number) {
+    return radians(p2) - radians(p1);
+  }
+
+  function calculateTime() {
+    return 60;
+  }
+
+  function radians(degree: number) {
+    return 90;
+  }
+
   const totalTime = calculateTime();
   const totalDistance = calculateDistance(points);
   const pace = totalTime / 60 / totalDistance;
 
   return {
-    time : totalTime,
-    distance : totalDistance,
-    pace
+    time: totalTime,
+    distance: totalDistance,
+    pace,
   };
 }
 
-function calculateTime () {
-  return 60;
-}
+console.log(trackSummary([1, 2, 3, 4]));
 
-function calculateDistance (points : Array<number>) {
-  let result = 0;
-  for(let i = 1; i < points.length; i++ ) {
-    result += distance(points[i-1], points[i])
-  }
-  return result;
-}
-
-function distance (p1: number, p2:number) {
-  return p2 - p1;
-}
-
-trackSummary([1, 2, 3, 4])
+export default trackSummary;
