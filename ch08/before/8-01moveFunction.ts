f1503e587480795fe8f86bb97d6d458c88c2160a
daal2quf <daal2quf@gmail.com>
2023-08-11 21:28:02 +0900

    feat: chapter8-01 추가 예제 코드 추가

--- ch08/before/8-01moveFunction.ts
+++ ch08/before/8-01moveFunction.ts
@@ -1,12 +1,5 @@
-function trackSummary(points: Array<number>) {
-  function calculateDistance(points: Array<number>) {
-    let result = 0;
-    for (let i = 1; i < points.length; i++) {
-      result += distance(points[i - 1], points[i]);
-    }
-    return result;
-  }
-
+// p280 : 중첩 함수를 최상위로 옮기기
+export function trackSummary(points: Array<number>) {
   function distance(p1: number, p2: number) {
     return radians(p2) - radians(p1);
   }
@@ -19,6 +12,15 @@ function trackSummary(points: Array<number>) {
     return 90;
   }
 
+  function calculateDistance(points: Array<number>) {
+    let result = 0;
+    for (let i = 1; i < points.length; i++) {
+      result += distance(points[i - 1], points[i]);
+    }
+    return result;
+  }
+
+  // 정말 확실하다 or 2차원배열 -> number[];
   const totalTime = calculateTime();
   const totalDistance = calculateDistance(points);
   const pace = totalTime / 60 / totalDistance;
@@ -32,4 +34,32 @@ function trackSummary(points: Array<number>) {
 
 console.log(trackSummary([1, 2, 3, 4]));
 
-export default trackSummary;
+interface TypeProps {
+  isPremium: boolean;
+}
+
+// p285 : 다른 클래스로 옮기기
+export class Account {
+  _daysOverdrawn;
+  type;
+
+  constructor(daysOverdrawn: number, type: TypeProps) {
+    this._daysOverdrawn = daysOverdrawn;
+    this.type = type;
+  }
+
+  get bankCharge() {
+    let result = 4.5;
+    if (this._daysOverdrawn > 0) return (result += this.overdraftCharge);
+    return result;
+  }
+
+  get overdraftCharge() {
+    if (this.type.isPremium) {
+      const baseCharge = 10;
+      if (this._daysOverdrawn <= 7) return baseCharge;
+      return baseCharge + (this._daysOverdrawn - 7) * 0.85;
+    }
+    return this._daysOverdrawn * 1.75;
+  }
+}
