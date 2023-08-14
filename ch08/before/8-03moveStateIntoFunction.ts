5742671879ce324b61125e01e59150434dac279a
daal2quf <daal2quf@gmail.com>
2023-08-14 21:56:54 +0900

    chapter08-3 initial code

---
+++ ch08/before/8-03moveStateIntoFunction.ts
@@ -0,0 +1,40 @@
+interface PhotoType {
+  title?: string;
+  location: string;
+  date: Date;
+  url: string;
+}
+
+interface PersonType {
+  name: string;
+  photo: PhotoType;
+  title?: string;
+}
+
+const photo = { title: '멋진 내 모습', location: '한강', date: new Date(), url: 'http://instagram.com/myNickName' };
+
+function renderPhoto(aPhoto: PhotoType) {
+  return `<img src="${aPhoto.url}" />`;
+}
+function emitPhotoData(aPhoto: PhotoType) {
+  const result = [];
+  result.push(`<p>위치: ${aPhoto.location}</p>`);
+  result.push(`<p>날짜: ${aPhoto.date.toDateString()}</p>`);
+  return result.join('\n');
+}
+
+function renderPerson(person: PersonType) {
+  const result = [];
+  result.push(`<p>${person.name}</p>`);
+  result.push(renderPhoto(person.photo));
+  result.push(`<p>제목: ${person.photo.title}</p>`);
+  result.push(emitPhotoData(person.photo));
+  return result.join('\n');
+}
+
+function photoDiv(person: PhotoType) {
+  ['<div>', `<p>제목: ${person.title}</p>`, emitPhotoData(person), '</div>'].join('\n');
+}
+
+console.log('** renderPerson **\n', renderPerson({ name: '현영', photo }));
+console.log('\n** photoDiv **\n', photoDiv(photo));
